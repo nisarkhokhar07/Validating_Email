619df07ba7457e392b5658dcfeebc6d46bba0663
nisarkhokhar07 <nisar.khokar@focusteck.com>
2023-07-18 17:20:24 +0500

    edited config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,16 @@
 // Object for Development Configuration
 const dotenv = require("dotenv");
 dotenv.config();
-console.log(process.env.DIALECT, process.env.HOST);
+// console.log(process.env.DIALECT, process.env.HOST);
 module.exports = {
   development: {
+    database: process.env.DATABASE,
+    username: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  },
+  production: {
     database: process.env.DATABASE,
     username: process.env.USER_NAME,
     password: process.env.PASSWORD,
